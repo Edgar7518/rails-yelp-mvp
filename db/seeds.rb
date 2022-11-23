3d843aab54def960e6a28c300bb5c960de84e360
edgar quenum <quenum.edgar@gmail.com>
2022-11-23 00:28:05 -0500

    yelp almost complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Nettoyons la DB..."
+
+Restaurant.destroy_all
+
+puts "Créons des restaurants.."
+
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Fantasy::Tolkien.character ,
+    address: Faker::Address.city,
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  puts "Restaurant #{restaurant.id} a été créé"
+end
+
+puts "Tout fini!"
